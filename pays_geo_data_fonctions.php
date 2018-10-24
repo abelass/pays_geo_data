a8691675cfe9a55572413bb5965de46c8bf1d638
abelass <rainer@websimple.be>
2018-10-24 11:55:12 +0200

    map fonctionne basiquement

--- pays_geo_data_fonctions.php
+++ pays_geo_data_fonctions.php
@@ -13,11 +13,15 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
+function balise_geometry_pays_dist($p) {
+	$p->code = '$Pile[$SP][\'geometry_pays\']';
+	return $p;
+}
 
 /*
  * Un fichier de fonctions permet de définir des éléments
  * systématiquement chargés lors du calcul des squelettes.
  *
  * Il peut par exemple définir des filtres, critères, balises, …
- * 
+ *
  */
--- pays_geo_data_pipelines.php
+++ pays_geo_data_pipelines.php
@@ -14,8 +14,16 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 }
 
 
-/*
- * Un fichier de pipelines permet de regrouper
- * les fonctions de branchement de votre plugin
- * sur des pipelines existants.
+/**
+ * Surcharger les boucles PAYS et celles qui comportent le critère gis
+ * pour permettre d'accéder à la valeur du champ geo au format WKT (voir balise #GEOMETRY_PAYS)
+ *
+ * @param $boucle
+ * @return mixed
  */
+function pays_geo_data_pre_boucle($boucle) {
+	if ($boucle->type_requete == 'pays' or in_array('pays', $boucle->jointures)) {
+		$boucle->select[]= 'AsText(pays.geo) AS geometry_pays';
+	}
+	return $boucle;
+}
